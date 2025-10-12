a07fff61815d7f5faf21ddcb033d160507e97264
ichikawa <bbbb_vanilla@icloud.com>
2025-10-12 17:42:24 +0900

    vercelデプロイテスト5

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
-if (!process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN) {
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
   throw new Error("MICROCMS_SERVICE_DOMAIN is required");
 }
 
@@ -9,6 +9,6 @@ if (!process.env.MICROCMS_API_KEY) {
 }
 
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.MICROCMS_API_KEY,
 });

a0599c7e3ef2fe986329887ea7d0612b7081fe33
ichikawa <bbbb_vanilla@icloud.com>
2025-10-15 21:42:12 +0900

    ボタンの文言修正

--- app/components/MoveButton.tsx
+++ app/components/MoveButton.tsx
@@ -26,7 +26,7 @@ const MoveButton = ({ area, className }) => {
   return (
     <button className={`btn bg-blue-500 text-white hover:bg-blue-400 border-none rounded-sm ${className}`} onClick={onClickArticle} data-area={area}>
       {area === "BlogBack" && "一覧へ戻る"}
-      {area === "BlogBack" || "一覧へ戻る"}
+      {area === "BlogBack" || "次へ進む"}
     </button>
   );
 };
